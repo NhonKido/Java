f560483e52b4df064817404fef731eaa5cc3e177
GaminBoiz <GaminBoiz@DESKTOP-MTVL1LH>
2025-12-27 01:26:11 +0700

    fix logic

--- Nhon/src/E5_7/IStore.java
+++ Nhon/src/E5_7/IStore.java
@@ -1,10 +1,9 @@
 package E5_7;
 
 public interface IStore {
-	double getPrice = 0;
+	public String brandList();
 	public double unitPrice();
 	public boolean lowerPrice(double amount);
 	public boolean cheaperThan(double cost);
-	public String brandList();
 	
 }
--- Nhon/src/E5_7/Store.java
+++ Nhon/src/E5_7/Store.java
@@ -10,6 +10,11 @@ public abstract class Store implements IStore {
 	@Override
 	public String brandList() {
 		return this.brand;
-	}
+	}	@Override
+	public double unitPrice() {return this.price/this.weight;}
+	@Override
+	public boolean lowerPrice(double amount) {return this.unitPrice() < amount;}
+	@Override
+	public boolean cheaperThan(double cost) {return this.price < cost;}
 	
 }
